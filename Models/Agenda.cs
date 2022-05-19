72a9824a91249a1350c977a109ae67115bc906fb
Danielcbrk <daniel_cm_br@hotmail.com>
2022-05-19 19:45:55 -0300

    Projeto-Base

---
+++ Models/Agenda.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+namespace atividade_3.Models
+{
+    public class Agenda
+    {
+        private static List<ItemAgenda> lista = new List<ItemAgenda>();
+
+        public static void Incluir(ItemAgenda item)
+        {
+            lista.Add(item);
+        }
+
+        public static List<ItemAgenda> Listar()
+        {
+            return lista;
+        }
+
+
+    }
+}
---
+++ Models/ItemAgenda.cs
@@ -0,0 +1,16 @@
+namespace atividade_3.Models
+{
+    public class ItemAgenda
+    {
+        public string nome { get; set; }
+        public string telefone { get; set; }
+        public string data { get; set; }
+        public string animal { get; set; }
+        public string necessidade { get; set; }
+        public string banho { get; set; }
+        public string tosa { get; set; }
+        public string consulta { get; set; }
+
+
+    }
+}
